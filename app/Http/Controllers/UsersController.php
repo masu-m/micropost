d6c29d77c968446478a21f67667f8615bce7bd12
masumi.shimizu <ebimo.72@gmail.com>
2019-09-18 08:15:03 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Users;
+use App\User;
 
 class UsersController extends Controller
 {
@@ -20,9 +20,15 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        
-        return view('users.show', [
-            'user' => $user, 
-        ]);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
